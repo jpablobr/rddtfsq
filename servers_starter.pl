13ab5b0d0f6c11caf443b9ddc8d926a593a9605c
jpablobr <xjpablobrx@gmail.com>
2011-03-16 11:11:15 +1000

    proper output redirect to /dev/null

--- servers_starter.pl
+++ servers_starter.pl
@@ -5,8 +5,8 @@ use strict;
 my $app_name = 'rddtfsq.pl';
 my $solr_path = 'solr/example';
 
-system("perl $app_name daemon &");
-system("cd $solr_path && java -jar start.jar &");
+system("perl $app_name daemon > /dev/null 2>&1 &");
+system("cd $solr_path && java -jar start.jar > /dev/null 2>&1 &");
 
 print("both server started successfully!");
 
